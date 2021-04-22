314fa9c74cb1db4710f8eac515bd7fe45a3d401b
harshrai654 <harshrai654@gmail.com>
2021-04-22 23:03:13 +0530

    Apply alert added

--- src/Components/Freelancer/ProjectCard.js
+++ src/Components/Freelancer/ProjectCard.js
@@ -1,28 +1,47 @@
-import { Card, Tag, Collapse, Button } from "antd";
+import { Card, Tag, Collapse, Button, Alert } from "antd";
 import {
   ExclamationCircleOutlined,
   CheckCircleOutlined,
 } from "@ant-design/icons";
 import utils from "../../utils";
+import { useState } from "react";
 
 const { Panel } = Collapse;
 const ProjectCard = (props) => {
   const proj = props.proj;
   const freelancer = props.freelancer;
   const token = props.token;
+  const [successAlert, setSuccessAlert] = useState(-1);
+
   const apply = (projectID) => {
     utils
       .applyProject(projectID, freelancer, token)
       .then((response) => {
-        console.log(response);
+        setSuccessAlert(1);
       })
       .catch((err) => {
-        console.log(err);
+        setSuccessAlert(0);
       });
   };
 
   return (
     <Card hoverable>
+      {successAlert == 1 && (
+        <Alert
+          closable
+          message="Applied successfully!"
+          type="success"
+          onClose={() => setSuccessAlert(-1)}
+        />
+      )}
+      {successAlert == 0 && (
+        <Alert
+          closable
+          message="Something went wrong!"
+          type="error"
+          onClose={() => setSuccessAlert(-1)}
+        />
+      )}
       <Tag
         icon={
           !proj.assigned ? (
@@ -47,10 +66,7 @@ const ProjectCard = (props) => {
           >
             Apply
           </Button>
-          <Button
-            type="primary"
-            disabled={proj.freelancer !== freelancer}
-          >
+          <Button type="primary" disabled={proj.freelancer !== freelancer}>
             View
           </Button>
         </Panel>
